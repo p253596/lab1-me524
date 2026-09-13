05035cc1450aec522ec9c6b38183d85372895b5c
p253596 <p253596@dac.unicamp.br>
2026-09-13 10:37:26 -0300

    avanços da bia

--- lab1.R
+++ lab1.R
@@ -1,3 +1,40 @@
 library(tidyverse)
 
-dados <- read_csv("brasileirao_2026.csv")
+dados <- read_csv("https://raw.githubusercontent.com/p253596/lab1-me524/refs/heads/main/brasileirao_2026.csv")
+
+head(dados)
+str(dados)
+summary(dados)
+tail(dados)
+
+jogos_realizados <- dados %>%
+  filter(!is.na(gols_mandante) & !is.na(gols_visitante))
+
+nrow(jogos_realizados)
+
+jogos_futuro <- dados %>%
+  filter(is.na(gols_mandante) | is.na(gols_visitante))
+
+nrow(jogos_futuro)
+
+# médias dos mandantes
+medias_mandante <- jogos_realizados %>%
+  group_by(time = time_mandante) %>%
+  summarise(
+    theta_m = mean(gols_mandante),
+    phi_m = mean(gols_visitante)
+  )
+
+# médias dos visitantes
+medias_visitante <- jogos_realizados %>%
+  group_by(time = time_visitante) %>%
+  summarise(
+    theta_v = mean(gols_visitante),
+    phi_v = mean(gols_mandante)
+  )
+
+# tabela dos parâmetros
+parametros_times <- medias_mandante %>%
+  inner_join(medias_visitante, by = "time")
+
+print(parametros_times)
